d50e2229cd1f583a93ff3ea223f443b6b202691e
Andres <andres.porfirio@gmail.com>
2022-11-30 14:11:05 -0300

    added navigator and home screen

---
+++ src/Router/index.tsx
@@ -0,0 +1,15 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../screens/Home";
+
+const Stack = createNativeStackNavigator();
+
+export default function Router() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
